8f049f8191415844847f428fde3f8992fed93e59
Evgeniy Mikhailov <jekkka23@mail.ru>
2024-08-09 16:32:19 +0500

    add notifications

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -22,7 +22,7 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager"; //стратегия загрузки, если страница долго грузится, с ее помощью мы не будем дожидаться полной загрузки страницы
         Configuration.baseUrl = "https://demoqa.com"; //выносим основной сайт, а в тестах уже оставляем только конкретный путь к сайту
-        Configuration.holdBrowserOpen = false; //делает браузер закрытым после теста (true оставляет открытым)
+        //Configuration.holdBrowserOpen = false; //делает браузер закрытым после теста (true оставляет открытым)
         //Configuration.timeout = 5000; по дефолту таймаут 4000
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub"; //для удаленного запуска
         DesiredCapabilities capabilities = new DesiredCapabilities();
